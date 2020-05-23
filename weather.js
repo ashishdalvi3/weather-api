339d0bf180ce4db86b479beaf7ecd6d592d7113e
Ashu <ashishdalvi3@gmail.com>
2020-05-23 12:39:39 +0530

    Initial commit

---
+++ weather.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = 'd9badd1b9b80090c303ae062a140f6e2';
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  //res.render('index');
+  //console.log(req.body.city);
+
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  request(url, function (err, response, body) {
+    let weather = JSON.parse(body)
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      
+      console.log(weather);
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees Fahrenheit in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+
+
+})
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
