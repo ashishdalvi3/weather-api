4768410f67998d327a2d99dff820511878dbd486
Ashu <ashishdalvi3@gmail.com>
2020-05-24 17:28:42 +0530

    add sec port

--- weather.js
+++ weather.js
@@ -38,6 +38,6 @@ app.post('/', function (req, res) {
 })
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
